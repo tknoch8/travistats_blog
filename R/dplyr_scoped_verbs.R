4c9e27bad5baa1794ad77b386745d554bf3be7fe
Travis.Knoche <travis.knoche@polarisalpha.com>
2019-06-02 12:52:06 -0600

    20190601

--- R/dplyr_scoped_verbs.R
+++ R/dplyr_scoped_verbs.R
@@ -1,3 +1,21 @@
 require(tidyverse)
 data(mtcars)
 glimpse(mtcars)
+
+test <- mtcars %>% 
+  mutate_all(.funs = list(~as.character(.)))
+
+glimpse(test)
+
+test <- mtcars %>% 
+  rename_all(.funs = list(~paste0("my_variable_", .)))
+
+glimpse(mtcars)
+
+test <- mtcars %>% 
+  mutate_at(vars(starts_with("c")), list(~ ./ 100))
+
+data("mpg")
+
+test <- mpg %>% 
+  mutate_if(is.double, list(~ as.integer(.)))
